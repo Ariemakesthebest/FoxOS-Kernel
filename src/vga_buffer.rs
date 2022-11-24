194165fdcf8eb0ee6867dd5f12f1ce25e0beeca9
Ariel Karson <arie45386@gmail.com>
2022-11-24 03:13:14 -0500

    Printing with extra steps is no MORE!

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,5 @@
 use volatile::Volatile;
+use core::fmt::{Write, Result};
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -89,7 +90,15 @@ impl Writer {
     }
 }
 
+impl Write for Writer {
+    fn write_str(&mut self, s: &str) -> Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
 pub fn print_something() {
+    
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
@@ -98,5 +107,5 @@ pub fn print_something() {
 
     writer.write_byte(b'H');
     writer.write_string("ello ");
-    writer.write_string("Wörld!");
+    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap()
 }
